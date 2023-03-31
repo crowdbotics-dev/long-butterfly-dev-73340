ab45bba339ebc8d6796955f86f8123930ea9a8a7
Crowdbotics <engineering@crowdbotics.com>
2023-03-31 01:10:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-butterfly-dev-73340",
+  url: "https://long-butterfly-dev-73340.botics.co",
+  api: "https://long-butterfly-dev-73340.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
